b89c57e1e43b32260181b606baf9b4c9ef9045e3
nicolasances <nicolas.matteazzi@gmail.com>
2025-11-15 12:55:27 +0100

    Dynamic consumer url

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,10 @@ const app = express();
 app.use(express.json());
 
 const PORT = 8000;
-// const CONSUMER_URL = "http://localhost:8081/galebroker/events/agent"
-const CONSUMER_URL = "http://localhost:8080/tomeagents/events/topic"
+const DEFAULT_CONSUMER_URL = "http://localhost:8080/tomeagents/events/topic"
+const CONSUMER_URL_MAP: { [key: string]: string } = {
+    task: "http://localhost:8081/galebroker/events/agent",
+}
 
 const queue: QueueMessage[] = [];
 
@@ -20,11 +22,14 @@ app.post('/msg', (req: Request, res: Response) => {
 
     const authHeader = req.get('authorization');
 
+    console.log(authHeader);
+    
+
     if (!authHeader) {
         res.status(403).json({ error: 'Missing Authorization header' });
         return;
     }
-    
+
     queue.push({
         payload: req.body,
         attempts: 0,
@@ -44,7 +49,7 @@ async function processQueue(): Promise<void> {
     // Process all available messages in parallel
     while (queue.length > 0) {
         const msg = queue.shift()!;
-        
+
         // Process this message without blocking others
         processMessage(msg).catch(err => {
             console.error("Unexpected error in processMessage:", err);
@@ -54,15 +59,22 @@ async function processQueue(): Promise<void> {
 
 async function processMessage(msg: QueueMessage): Promise<void> {
     try {
-        console.log(`Delivering message to endpoint ${CONSUMER_URL}`);
-        
-        await axios.post(CONSUMER_URL, msg.payload, {
+
+        const msgType = msg.payload.type;
+
+        // Find the consumer URL 
+        let consumerURL = DEFAULT_CONSUMER_URL;
+        if (CONSUMER_URL_MAP[msgType]) consumerURL = CONSUMER_URL_MAP[msgType];
+
+        console.log(`Delivering message to endpoint ${consumerURL}`);
+
+        await axios.post(consumerURL, msg.payload, {
             timeout: 600000,
             headers: {
                 Authorization: msg.authHeader
             }
         });
-        
+
         console.log("Delivered successfully");
 
     } catch (err: any) {
@@ -72,15 +84,15 @@ async function processMessage(msg: QueueMessage): Promise<void> {
         msg.attempts = (msg.attempts ?? 0) + 1;
 
         if (msg.attempts < 3) {
-            
+
             console.log("Retrying later...");
-            
+
             queue.push(msg);
-            
+
             // Retry after delay
             setTimeout(processQueue, 1000);
 
-        } 
+        }
         else {
             console.error("Dropping message after 3 failed attempts");
         }
@@ -88,5 +100,5 @@ async function processMessage(msg: QueueMessage): Promise<void> {
 }
 
 app.listen(PORT, () => {
-  console.log(`Queue service running on port ${PORT}`);
+    console.log(`Queue service running on port ${PORT}`);
 });
